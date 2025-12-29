0590cf4c9f6eaa85a90e190bbef597583af72d4f
Younis202 <younismohamed87643@gmail.com>
2025-12-29 04:37:35 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import LaureatesPage from "./pages/LaureatesPage";
-import CategoriesPage from "./pages/CategoriesPage";
-import ResourcesPage from "./pages/ResourcesPage";
-import AboutPage from "./pages/AboutPage";
+import Projects from "./pages/projectsPage/Projects";
+import Services from "./pages/servicesPage/Services";
+import About from "./pages/aboutPage/About";
+import Contact from "./pages/contactPage/Contact";
+import ServiceOne from "./pages/service-one/Service-one";
+import ServiceTwo from "./pages/service-two/Service-two";
+import ServiceThree from "./pages/service-three/Service-three";
+import ProjectOne from "./pages/project-one/Project-one";
+import ProjectTwo from "./pages/project-two/Project-two";
+import BookACall from "./pages/bookPage/book-a-call";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/laureates" element={<LaureatesPage />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/resources" element={<ResourcesPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/project-one" element={<ProjectOne />} />
+        <Route path="/project-two" element={<ProjectTwo />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/service-one" element={<ServiceOne />} />
+        <Route path="/service-two" element={<ServiceTwo />} />
+        <Route path="/service-three" element={<ServiceThree />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/book-a-call" element={<BookACall />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
